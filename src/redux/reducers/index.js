9295352add84dd6893660220e55631ec40a50758
mark sauer-utley <mark.utley.713@gmail.com>
2020-03-30 20:24:02 -0400

    fix logout action

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,12 +1,19 @@
 import { combineReducers } from 'redux';
+import rootReducer from './root';
 import authReducer from './auth';
 import charactersReducer from './characters';
 import searchReducer from './search';
 import newReducer from './new';
 
-export default combineReducers({
+const sliceReducer = combineReducers({
 	auth: authReducer,
 	characters: charactersReducer,
 	search: searchReducer,
 	new: newReducer,
 });
+
+export default (state, action) => {
+	const intermediateState = sliceReducer(state, action);
+	const finalState = rootReducer(intermediateState, action);
+	return finalState;
+};
