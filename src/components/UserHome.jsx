8d8ddd9add70de3e1f26094c6f8da227813d2db6
mark sauer-utley <mark.utley.713@gmail.com>
2020-04-20 20:15:44 -0400

    update homepage styles

--- src/components/UserHome.jsx
+++ src/components/UserHome.jsx
@@ -1,11 +1,9 @@
 import Link from 'next/link';
-import { useSelector } from 'react-redux';
 import HomeLayout from './layouts/HomeLayout';
 import useCharacters from '../hooks/useCharacters';
 import Characters from './Characters';
 
 const UserHome = () => {
-	const name = useSelector((s) => s.auth.user?.name);
 	const { data, error } = useCharacters();
 
 	return (
@@ -14,7 +12,6 @@ const UserHome = () => {
 			{
 				data && (
 					<>
-						<h1>Hello {name}</h1>
 						{data.length === 0 ? (
 							<p><Link href="/characters/new"><a>Go here</a></Link> to create your fist character.</p>
 						) : <Characters />}
